8f729d143351eee5c54374d6e4de32b785c05ea6
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-05-09 15:53:10 +0530

    task: prepare audio files for speech-to-text transcription

--- utils/audio.py
+++ utils/audio.py
@@ -10,15 +10,17 @@ def download_audio(url : str) -> str:
   output_path = os.path.join(DOWNLOAD_DIRECTORY, "%(title)s.%(ext)s")
   ydl_opts = {
     "format": "bestaudio/best",
-    "outtmpl" : output_path,
-    "postprocessors" : [
+    "outtmpl": output_path,
+    "noplaylist": True,
+    "quiet": False,
+    "cookiefile": "cookies.txt",
+    "postprocessors": [
       {
-        "key" : "FFmpegExtractAudio",
+        "key": "FFmpegExtractAudio",
         "preferredcodec": "wav",
-        "preferredquality" : "192"
+        "preferredquality": "192",
       }
     ],
-    "quiet" : True
   }
   
   with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -27,4 +29,13 @@ def download_audio(url : str) -> str:
   return filename
 
 
-print(download_audio("https://www.youtube.com/watch?v=xlYJhtL0qbQ&t=554s"))
+data = download_audio("https://www.youtube.com/watch?v=3GiS5xJoYRE")
+
+def convert_to_wav(input : str) -> str:
+  output_path = os.path.splitext(input)[0] + "_converted.wav"
+  audio = AudioSegment.from_file(input)
+  audio = audio.set_channels(1).set_frame_rate(16000)
+  audio.export(output_path, format="wav")
+  return output_path
+
+print(convert_to_wav(data))
