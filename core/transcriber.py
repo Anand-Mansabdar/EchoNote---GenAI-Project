bf5cc679bc4c9d1a8ab09a71667fa977d43402b1
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-05-09 18:12:25 +0530

    task: Function to convert the audio to text(transcribing)

--- core/transcriber.py
+++ core/transcriber.py
@@ -14,3 +14,26 @@ def load_model():
     _model = whisper.load_model(whisper_model)
     print(f"Whisper Model loaded successfully...")
   return _model
+
+
+# Transcribe a single chunk
+def transcribe_chunk(chunk_path: str, translate: bool = False) -> str:
+  model = load_model()
+  task = "translate" if translate else "transcribe"
+  
+  result = model.transcribe(chunk_path, task = task)
+  
+  return result["text"]
+
+# Transscribing multiple chunks
+def transcribe_all_chunks(chunks: list, translate: bool = False) -> str:
+  final_transcription = ""
+  
+  for i, chunk in enumerate(chunks):
+    print(f"Transcribing Chunk {i+1}")
+    text = transcribe_chunk(chunk_path= chunk, translate = translate)
+    
+    final_transcription += text + " "
+  
+  print("Transcription Completed")
+  return final_transcription
